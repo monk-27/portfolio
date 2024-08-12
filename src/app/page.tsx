a326cc6af8d13a85516922a4bda407cb1bea9a1c
armaanjaj <armaan.jaj@gmail.com>
2024-08-11 18:29:36 -0600

    Redesign Homepage
    Remove the two column design. Design the home page from scratch.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,224 @@
-import Arrangement from "@/components/layout/Arrangement";
+"use client";
+import ActionButton from "@/components/layout/ActionButton";
+import ProjectCard from "@/components/layout/ProjectCard";
+import DynamicNavbar from "@/components/ui/DynamicNavbar";
+import Footer from "@/components/ui/Footer";
+import { ActiveRepo, Contributions, Languages } from "@/components/ui/GitHub";
+import Heading from "@/components/ui/Heading";
+import Paragraph from "@/components/ui/Paragraph";
+import Testimonials from "@/components/ui/Testimonials";
+import gsap from "gsap";
+import React from "react";
+import { IoReload } from "react-icons/io5";
 
 export default function Home() {
+    return (
+        <main className="h-full w-full bg-white">
+            <DynamicNavbar title="Armaan Jaj" className="bg-white" />
+            <HeroSection />
+            <AnalyticsSection />
+            <ProjectsSection />
+            <section className="bg-white py-10 max-w-[90vw] lg:max-w-[70vw] mx-auto">
+                <Testimonials />
+            </section>
+            <Footer />
+        </main>
+    );
+}
+
+function HeroSection() {
+    return (
+        <section className="relative w-full py-20 px-5 bg-white text-center">
+            <div className="container mx-auto flex flex-col items-center justify-center">
+                <Heading className="text-3xl md:text-5xl font-bold leading-tight">
+                    Crafting Digital Experiences with{" "}
+                    <span className="text-aqua-green whitespace-nowrap">
+                        Artistry & Precision
+                    </span>
+                </Heading>
+                <Paragraph className="text-base md:text-lg leading-relaxed mt-4 max-w-3xl">
+                    Empowering your business with top-notch web solutions. Let's
+                    create something extraordinary together.
+                </Paragraph>
+                <div className="flex gap-3 md:gap-5 mt-8 justify-center">
+                    <ActionButton text="View My Work" size="md" />
+                    <ActionButton text="Hire Me" size="md" />
+                </div>
+            </div>
+        </section>
+    );
+}
+
+function AnalyticsSection() {
+    const [reload, setReload] = React.useState(false);
+
+    const handleReload = () => {
+        setReload((prevReload) => !prevReload);
+    };
 
     return (
-        <main className="h-full w-full">
-            <Arrangement />
-        </main>
+        <section className="py-16 px-5 bg-gray-50">
+            <div className="container mx-auto">
+                <Heading className="text-3xl md:text-4xl font-bold text-center mb-10">
+                    Activity Hub
+                </Heading>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8">
+                    <div className="bg-aqua-green bg-opacity-50 p-6 rounded-lg flex flex-col justify-between items-center">
+                        <div className="w-full flex justify-between items-center">
+                            <h3 className="text-xl md:text-2xl font-bold">
+                                Active Project
+                            </h3>
+                            <div
+                                className="text-gray-500 cursor-pointer hover:text-aqua-green transition duration-300"
+                                onClick={handleReload}
+                                title="Reload repo data"
+                            >
+                                <IoReload size={24} />
+                            </div>
+                        </div>
+                        <div className="mt-5 w-full flex-grow">
+                            <ActiveRepo reload={reload} />
+                        </div>
+                    </div>
+                    <div className="bg-white p-6 rounded-lg flex flex-col justify-between">
+                        <h3 className="text-xl md:text-2xl font-bold">
+                            GitHub Contributions
+                        </h3>
+                        <div className="mt-4 flex-grow">
+                            <Contributions />
+                        </div>
+                    </div>
+                    <div className="bg-white p-6 rounded-lg flex flex-col justify-between">
+                        <h3 className="text-xl md:text-2xl font-bold">
+                            Top Languages
+                        </h3>
+                        <div className="mt-4 flex-grow">
+                            <Languages />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+interface Project {
+    id: number;
+    title: string;
+    description: string;
+    logo: string;
+    url: string;
+}
+
+interface IProjectsProps {
+    className?: string;
+}
+
+function ProjectsSection({ className }: IProjectsProps) {
+    const projectsRef = React.useRef<HTMLDivElement | null>(null);
+    const [projects, setProjects] = React.useState<Project[]>([]);
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        const fetchProjects = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch("/api/projects");
+                if (response.ok) {
+                    const data: Project[] = await response.json();
+                    setProjects(data);
+                } else {
+                    console.error("Failed to fetch projects");
+                }
+            } catch (error) {
+                console.error("Error:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProjects();
+    }, []);
+
+    React.useLayoutEffect(() => {
+        if (projectsRef.current) {
+            const cards = projectsRef.current.querySelectorAll(".project-card");
+
+            if (cards.length > 0) {
+                cards.forEach((card) => {
+                    const tiltX = Math.random() * 30 - 10;
+                    const tiltY = Math.random() * 30 - 10;
+
+                    // Apply initial tilt
+                    gsap.set(card, { rotateX: tiltX, rotateY: tiltY });
+
+                    // Hover animation to reset tilt
+                    card.addEventListener("mouseenter", () => {
+                        gsap.to(card, {
+                            rotateX: 0,
+                            rotateY: 0,
+                            duration: 0.5,
+                            ease: "power3.out",
+                        });
+                    });
+
+                    // Return to tilt when hover ends
+                    card.addEventListener("mouseleave", () => {
+                        gsap.to(card, {
+                            rotateX: tiltX,
+                            rotateY: tiltY,
+                            duration: 0.5,
+                            ease: "power3.out",
+                        });
+                    });
+                });
+
+                // Fade in the project cards when the page changes
+                gsap.fromTo(
+                    cards,
+                    { opacity: 0, x: -100 },
+                    {
+                        opacity: 1,
+                        x: 0,
+                        duration: 0.4,
+                        stagger: 0.1,
+                        ease: "power2.inOut",
+                    }
+                );
+            }
+        }
+    }, [projects]);
+
+    return (
+        <section className="py-20 px-5 bg-gray-50">
+            <div className="container mx-auto">
+                <h2 className="text-3xl md:text-4xl font-bold text-center mb-10">
+                    Recent Projects
+                </h2>
+                <div
+                    ref={projectsRef}
+                    className={`${className} grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-8`}
+                >
+                    {loading ? (
+                        <div className="col-span-full flex justify-center items-center h-64">
+                            <span className="text-base animate-pulse">
+                                Loading...
+                            </span>
+                        </div>
+                    ) : (
+                        projects.map((item) => (
+                            <div key={item.id} className="project-card">
+                                <ProjectCard
+                                    title={item.title}
+                                    description={item.description}
+                                    logo={item.logo}
+                                    url={item.url}
+                                />
+                            </div>
+                        ))
+                    )}
+                </div>
+            </div>
+        </section>
     );
 }
---
+++ src/components/ui/GitHub.tsx
@@ -0,0 +1,200 @@
+"use client";
+import * as React from "react";
+import FolderCard from "../layout/FolderCard";
+import CustomLineChart from "../charts/CustomLineChart";
+import CustomPieChart from "../charts/CustomPieChart";
+
+export function ActiveRepo({
+    className,
+    reload,
+}: {
+    className?: string;
+    reload: boolean;
+}) {
+    const [repoData, setRepoData] = React.useState({
+        title: "",
+        description: "",
+        starCount: 0,
+        forkCount: 0,
+        isPrivate: false,
+        language: "", // Updated to store a single language
+        latestCommitMessage: "",
+        latestCommitUrl: "",
+        latestCommitDate: "",
+    });
+
+    const [isLoading, setIsLoading] = React.useState(true);
+    const [error, setError] = React.useState<string | null>(null);
+
+    React.useEffect(() => {
+        const fetchLatestCommitData = async () => {
+            setIsLoading(true);
+            try {
+                const owner = "armaanjaj";
+
+                const response = await fetch(
+                    `/api/github-latest-commit?owner=${owner}`
+                );
+
+                if (!response.ok) {
+                    throw new Error("Failed to fetch repository details");
+                }
+
+                const data = await response.json();
+
+                setRepoData({
+                    title: data.title,
+                    description: data.description || "No Description",
+                    starCount: data.starCount || 0,
+                    forkCount: data.forkCount || 0,
+                    isPrivate: data.isPrivate,
+                    language: data.language || "N/A", // Updated to handle a single language
+                    latestCommitMessage: data.latestCommitMessage,
+                    latestCommitUrl: data.latestCommitUrl,
+                    latestCommitDate: new Date(
+                        data.latestCommitDate
+                    ).toLocaleString(),
+                });
+            } catch (err: any) {
+                setError(err.message || "An unknown error occurred");
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchLatestCommitData();
+    }, [reload]); // Depend on reload to refetch data when it changes
+
+    if (isLoading) {
+        return (
+            <p className="set-wf-full flex-col-center">
+                Loading repo details...
+            </p>
+        );
+    }
+
+    if (error) {
+        return <p className="set-wf-full flex-col-center">{error}</p>;
+    }
+
+    return (
+        <div className={`set-wf-full flex-col-center ${className}`}>
+            <FolderCard
+                title={repoData.title}
+                description={repoData.description}
+                starCount={repoData.starCount}
+                forkCount={repoData.forkCount}
+                isPrivate={repoData.isPrivate}
+                language={repoData.language} // Pass the single language
+                latestCommitMessage={repoData.latestCommitMessage}
+                latestCommitUrl={repoData.latestCommitUrl}
+                latestCommitDate={repoData.latestCommitDate}
+            />
+        </div>
+    );
+}
+
+export function Contributions() {
+    const [year, setYear] = React.useState("2024");
+    const [contributions, setContributions] = React.useState([]);
+    const [totalContributions, setTotalContributions] = React.useState(0);
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState(null);
+
+    const years = ["2022", "2023", "2024"];
+
+    const fetchContributionsForYear = async (selectedYear: string) => {
+        setLoading(true);
+        setError(null);
+
+        const from = `${selectedYear}-01-01T00:00:00Z`;
+        const to = `${selectedYear}-12-31T23:59:59Z`;
+
+        try {
+            const response = await fetch(
+                `/api/github-contributions?username=armaanjaj&from=${from}&to=${to}`
+            );
+            if (!response.ok) throw new Error("Failed to fetch data");
+
+            const result = await response.json();
+            setContributions(result.contributions);
+            setTotalContributions(result.totalContributions);
+            setYear(selectedYear);
+        } catch (err: any) {
+            setError(err.message || "An unknown error occurred");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    React.useEffect(() => {
+        fetchContributionsForYear(year);
+    }, [year]);
+
+    return (
+        <section className="flex-col-start min-h-full">
+            {loading ? (
+                <p className="set-wf-full flex-col-center">
+                    {loading ? "Loading GitHub contributions..." : error}
+                </p>
+            ) : (
+                <CustomLineChart
+                    contributions={contributions}
+                    totalContributions={totalContributions}
+                    year={year}
+                    years={years}
+                    onYearChange={fetchContributionsForYear}
+                />
+            )}
+        </section>
+    );
+}
+
+export function Languages() {
+    const [languageData, setLanguageData] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState<string | null>(null);
+
+    const fetchLanguagesData = async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch(
+                `/api/github-languages?username=armaanjaj`
+            );
+            if (!response.ok) throw new Error("Failed to fetch data");
+
+            const result = await response.json();
+
+            const languages = result.map((item: any) => ({
+                name: item.language || item.name || "Unknown",
+                size: item.size || 0,
+            }));
+
+            setLanguageData(languages);
+        } catch (err: any) {
+            setError(err.message || "An unknown error occurred");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    React.useEffect(() => {
+        fetchLanguagesData();
+    }, []);
+
+    if (loading) {
+        return (
+            <p className="set-wf-full flex-col-center">
+                Loading language data...
+            </p>
+        );
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    return <CustomPieChart data={languageData} />;
+}
