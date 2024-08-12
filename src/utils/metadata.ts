2b3d213ed023a93d211c533674375abeb0aafe23
armaanjaj <armaan.jaj@gmail.com>
2024-08-12 15:16:09 -0600

    Restructure metadata
    Make new utils to manage metadata at a single place.

---
+++ src/utils/metadata.ts
@@ -0,0 +1,118 @@
+export const homepage_MD = {
+    title: "Armaan Jaj | Developer Portfolio",
+    description:
+        "Explore the portfolio of Armaan Jaj, a full-stack web developer with a passion for crafting high-performance, visually stunning, and user-centric web solutions. Discover innovative projects and case studies that blend creativity with technical excellence.",
+    keywords:
+        "Armaan Jaj, web developer, full stack developer, portfolio, web development, front-end, back-end, JavaScript, React, Next.js, Node.js, software engineering",
+    url: "https://www.armaancodes.com",
+    images: [
+        {
+            url: "https://www.armaancodes.com/website-gallery/homepage.png",
+            width: 1200,
+            height: 630,
+            alt: "Armaan Jaj | Developer Portfolio - Home page",
+        },
+    ],
+};
+
+export const aboutpage_MD = {
+    title: "About me - Armaan Jaj | Developer Portfolio",
+    description:
+        "Explore the portfolio of Armaan Jaj, a full-stack web developer with a passion for crafting high-performance, visually stunning, and user-centric web solutions. Discover innovative projects and case studies that blend creativity with technical excellence.",
+    keywords:
+        "Armaan Jaj, web developer, full stack developer, portfolio, web development, front-end, back-end, JavaScript, React, Next.js, Node.js, software engineering",
+    url: "https://www.armaancodes.com",
+    images: [
+        {
+            url: "https://www.armaancodes.com/website-gallery/about.png",
+            width: 1200,
+            height: 630,
+            alt: "Armaan Jaj | Developer Portfolio - About page",
+        },
+    ],
+};
+
+export const servicespage_MD = {
+    title: "Services - Armaan Jaj | Developer Portfolio",
+    description:
+        "Explore the portfolio of Armaan Jaj, a full-stack web developer with a passion for crafting high-performance, visually stunning, and user-centric web solutions. Discover innovative projects and case studies that blend creativity with technical excellence.",
+    keywords:
+        "Armaan Jaj, web developer, full stack developer, portfolio, web development, front-end, back-end, JavaScript, React, Next.js, Node.js, software engineering",
+    url: "https://www.armaancodes.com",
+    images: [
+        {
+            url: "https://www.armaancodes.com/website-gallery/services.png",
+            width: 1200,
+            height: 630,
+            alt: "Armaan Jaj | Developer Portfolio - Services page",
+        },
+    ],
+};
+
+export const toolspage_MD = {
+    title: "Tools - Armaan Jaj | Developer Portfolio",
+    description:
+        "Explore the portfolio of Armaan Jaj, a full-stack web developer with a passion for crafting high-performance, visually stunning, and user-centric web solutions. Discover innovative projects and case studies that blend creativity with technical excellence.",
+    keywords:
+        "Armaan Jaj, web developer, full stack developer, portfolio, web development, front-end, back-end, JavaScript, React, Next.js, Node.js, software engineering",
+    url: "https://www.armaancodes.com",
+    images: [
+        {
+            url: "https://www.armaancodes.com/website-gallery/tools.png",
+            width: 1200,
+            height: 630,
+            alt: "Armaan Jaj | Developer Portfolio - Tools page",
+        },
+    ],
+};
+
+export const termspage_MD = {
+    title: "Terms of Service - Armaan Jaj | Developer Portfolio",
+    description:
+        "Explore the portfolio of Armaan Jaj, a full-stack web developer with a passion for crafting high-performance, visually stunning, and user-centric web solutions. Discover innovative projects and case studies that blend creativity with technical excellence.",
+    keywords:
+        "Armaan Jaj, web developer, full stack developer, portfolio, web development, front-end, back-end, JavaScript, React, Next.js, Node.js, software engineering",
+    url: "https://www.armaancodes.com",
+    images: [
+        {
+            url: "https://www.armaancodes.com/website-gallery/homepage.png",
+            width: 1200,
+            height: 630,
+            alt: "Armaan Jaj | Developer Portfolio - Home page",
+        },
+    ],
+};
+
+export const privacypolicy_MD = {
+    title: "Privacy policy - Armaan Jaj | Developer Portfolio",
+    description:
+        "Explore the portfolio of Armaan Jaj, a full-stack web developer with a passion for crafting high-performance, visually stunning, and user-centric web solutions. Discover innovative projects and case studies that blend creativity with technical excellence.",
+    keywords:
+        "Armaan Jaj, web developer, full stack developer, portfolio, web development, front-end, back-end, JavaScript, React, Next.js, Node.js, software engineering",
+    url: "https://www.armaancodes.com",
+    images: [
+        {
+            url: "https://www.armaancodes.com/website-gallery/homepage.png",
+            width: 1200,
+            height: 630,
+            alt: "Armaan Jaj | Developer Portfolio - Home page",
+        },
+    ],
+};
+
+export const sitemappage_MD = {
+    title: "Sitemap - Armaan Jaj | Developer Portfolio",
+    description:
+        "Explore the portfolio of Armaan Jaj, a full-stack web developer with a passion for crafting high-performance, visually stunning, and user-centric web solutions. Discover innovative projects and case studies that blend creativity with technical excellence.",
+    keywords:
+        "Armaan Jaj, web developer, full stack developer, portfolio, web development, front-end, back-end, JavaScript, React, Next.js, Node.js, software engineering",
+    url: "https://www.armaancodes.com",
+    images: [
+        {
+            url: "https://www.armaancodes.com/website-gallery/homepage.png",
+            width: 1200,
+            height: 630,
+            alt: "Armaan Jaj | Developer Portfolio - Home page",
+        },
+    ],
+};
